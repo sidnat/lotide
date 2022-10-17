5b183c4baaba1c0d9964efed64b36e85d0f68a77
Sid Natarajan <sid.nat@gmail.com>
2022-10-17 19:16:27 +0000

    assertion pass/fail function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual(1, "test");
+assertEqual("test", "t3st");
+assertEqual("one", "One");
+assertEqual("two", "two");
