51489997d7aad6ac4d196adecc435d523b14a876
Sid Natarajan <sid.nat@gmail.com>
2022-10-17 19:26:04 +0000

    compare head of arrays with assertion pass/fail function

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = (inputArr) => {
+  return inputArr[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello");
