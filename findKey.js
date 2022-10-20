b59d7b0ddf34b5611fbcaa44b8c11aa1be15d431
Sid Natarajan <sid.nat@gmail.com>
2022-10-20 22:43:24 +0000

    function will find and return the key of an object

---
+++ findKey.js
@@ -0,0 +1,42 @@
+//Implement the function findKey which takes in an object and a callback.
+//It should scan the object
+//return the first key for which the callback returns a truthy value.
+//If no key is found, then it should return undefined.
+
+const findKey = (object, callback) => {
+  let arrayOfKeys = Object.keys(object);
+
+  for (let key of arrayOfKeys) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+
+  return undefined;
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
