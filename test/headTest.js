3baa17ec83c6ac2f69d4339f8d7c0e4469cbf8c2
Sid Natarajan <sid.nat@gmail.com>
2022-10-24 19:13:29 +0000

    introduced mocha testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,10 +1,23 @@
-const assertEqual = require("../assertEqual");
-const head = require("../head");
+// const assertEqual = require("../assertEqual");
+// const head = require("../head");
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), "Hello");
 
-// ✅✅✅ Assertion Passed: 5 === 5
-// ✅✅✅ Assertion Passed: Hello === Hello
-// 🛑🛑🛑 Assertion Failed: undefined !== Hello
+// // ✅✅✅ Assertion Passed: 5 === 5
+// // ✅✅✅ Assertion Passed: Hello === Hello
+// // 🛑🛑🛑 Assertion Failed: undefined !== Hello
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
